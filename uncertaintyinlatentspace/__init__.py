d68c37e3f4cc1247a95c98be089105b13f145f51
Badr-Eddine Marani <badr-eddine.marani@michelin.com>
2023-03-27 17:26:20 +0200

    remove unnecessary files

--- uncertaintyinlatentspace/__init__.py
+++
@@ -1,8 +0,0 @@
-# coding: utf-8
-"""Main module of uncertaintyinlatentspace."""
-
-try:
-    from .version import __version__, __version_date__  # pylint: disable=import-error
-except ImportError:
-    __version__ = None
-    __version_date__ = ""
