12ec5c8e35a82296fcd9ee7bc5172a62cde575b9
Badr-Eddine Marani <badr-eddine.marani@michelin.com>
2023-04-03 15:08:35 +0200

    smallish update

--- experiments/prepare_dataset.py
+++ experiments/prepare_dataset.py
@@ -1,36 +1,45 @@
 import os
 import pandas as pd
 
-FILENAME = "cont_features.csv"
-OUTP_DIR = "data/cont_features_tr.csv"
+FILENAME = "data/fulldataset.csv"
+OUTP_DIR = "data/fulldataset_notna_targets_vnum0.csv"
 
-dataset = pd.read_csv(FILENAME, sep = ",")
+dataset = pd.read_csv(FILENAME, sep = ";")
 
 # Use only the first two targets in the dataset and remove unnecessary columns.
 # columns = ["Unnamed: 0", "ROW_ID"]
-# columns += [
-#     col for idx, col in enumerate(dataset.columns)
-#     if (
-#         col.startswith("target") and idx <= 1
-#     )
-# ]
 
-# dataset.drop(
-#     columns = columns,
-#     inplace = True,
-# )
+columns = [
+    "target_00"+str(i)
+    for i in range(2, 6)
+]
+
+dataset.drop(
+    columns = columns,
+    inplace = True,
+)
+
 
 # For now, I'll only consider rows where both targets are available.
 targets = dataset[["target_000", "target_001"]]
-notna_dataset = dataset[
+dataset = dataset[
     targets.notna().all(1)
 ]
 
-if not os.path.exists(OUTP_DIR):
-    os.makedirs("data", exist_ok=True)
-    notna_dataset.to_csv(
-        OUTP_DIR,
-        sep = ",",
-        header = True,
-        index = False,
-    )
+# Remove features with missing values.
+dataset.dropna(
+    axis="columns",
+    how="any",
+    inplace=True,
+)
+
+os.makedirs("data", exist_ok=True)
+if os.path.exists(OUTP_DIR):
+    os.remove(OUTP_DIR)
+
+dataset.to_csv(
+    OUTP_DIR,
+    sep = ",",
+    header = True,
+    index = False,
+)
