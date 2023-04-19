0b65dfe992bfa4c227b91a9a978371acb4446749
Badr-Eddine Marani <badr-eddine.marani@michelin.com>
2023-04-19 17:28:49 +0200

    wip: vae benchmark

--- experiments/vae_benchmark/main.py
+++ experiments/vae_benchmark/main.py
@@ -1,197 +1,169 @@
-import torch
-
-from torch.utils.tensorboard import SummaryWriter
-
-import importlib
 import datetime
+import decimal
+import gc
+import importlib
 import os
+import warnings
 
-from utils import Encoder, Decoder, TBLogger
-
+import pandas as pd
+import torch
+import yaml
 from pythae import models
+from pythae.data.datasets import BaseDataset
 from pythae.pipelines import TrainingPipeline
-from pythae.trainers import (
-    BaseTrainerConfig,
-    AdversarialTrainerConfig,
-    CoupledOptimizerAdversarialTrainerConfig,
-    CoupledOptimizerTrainerConfig,
-)
+from pythae.trainers import BaseTrainer, BaseTrainerConfig
+from ray import air, tune
+from ray.tune.schedulers import ASHAScheduler
+from torch.utils import data
+from torch.utils.tensorboard import SummaryWriter
+from torchvision import transforms
+from torchvision.datasets import MNIST
 
-inp_size = 20
-emb_sizes = [
-    inp_size//i
-    for i in range(1, 4)
-]
+from utils import *
 
-lat_size = 2
+warnings.filterwarnings("ignore")
 
-
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-# device = torch.device("cpu")
-
-XX = torch.randn(size=(6000, inp_size)).to(device)
-
-decoder = Decoder(lat_size, emb_sizes, inp_size).to(device)
-
-training_signature = str(datetime.datetime.now())[0:19].replace(" ", "_").replace(":", "-")
-output_dir = "experiments/vae_benchmark/all_models_logs"
-ts_output_dir = os.path.join(output_dir, "tensorboard_"+training_signature)
-os.makedirs(ts_output_dir, exist_ok=True)
-
-training_base_config = BaseTrainerConfig(
-    no_cuda = False,
-    num_epochs = 40,
-    keep_best_on_train = True,
-    output_dir = output_dir,
-    per_device_train_batch_size = 64,
-    per_device_eval_batch_size = 64,
-    optimizer_cls = "Adam",
-    learning_rate = 1e-4,
-    optimizer_params = {
-        "betas": (0.91, 0.995),
-        "weight_decay": 0.05,
-    },
-    scheduler_cls="ReduceLROnPlateau",
-    scheduler_params={"patience": 5, "factor": 0.5}
-)
-
-
-training_aae_config = AdversarialTrainerConfig(
-    no_cuda = False,
-    num_epochs = 1,
-    keep_best_on_train = True,
-    output_dir = output_dir,
-    per_device_train_batch_size = 64,
-    per_device_eval_batch_size = 64,
-    autoencoder_optimizer_cls = "Adam",
-    autoencoder_learning_rate = 1e-4,
-    autoencoder_optimizer_params = {
-        "betas": (0.91, 0.995),
-        "weight_decay": 0.05,
-    },
-    autoencoder_scheduler_cls="ReduceLROnPlateau",
-    autoencoder_scheduler_params={"patience": 5, "factor": 0.5},
-    discriminator_optimizer_cls = "Adam",
-    discriminator_learning_rate = 1e-4,
-    discriminator_optimizer_params = {
-        "betas": (0.91, 0.995),
-        "weight_decay": 0.05,
-    },
-    discriminator_scheduler_cls="ReduceLROnPlateau",
-    discriminator_scheduler_params={"patience": 5, "factor": 0.5}
-)
-
-training_vaegan_config = CoupledOptimizerAdversarialTrainerConfig(
-    no_cuda = False,
-    num_epochs = 1,
-    keep_best_on_train = True,
-    output_dir = output_dir,
-    per_device_train_batch_size = 64,
-    per_device_eval_batch_size = 64,
-    encoder_optimizer_cls = "Adam",
-    encoder_learning_rate = 1e-4,
-    encoder_optimizer_params = {
-        "betas": (0.91, 0.995),
-        "weight_decay": 0.05,
-    },
-    encoder_scheduler_cls="ReduceLROnPlateau",
-    encoder_scheduler_params={"patience": 5, "factor": 0.5},
-    decoder_optimizer_cls = "Adam",
-    decoder_learning_rate = 1e-4,
-    decoder_optimizer_params = {
-        "betas": (0.91, 0.995),
-        "weight_decay": 0.05,
-    },
-    decoder_scheduler_cls="ReduceLROnPlateau",
-    decoder_scheduler_params={"patience": 5, "factor": 0.5},
-    discriminator_optimizer_cls = "Adam",
-    discriminator_learning_rate = 1e-4,
-    discriminator_optimizer_params = {
-        "betas": (0.91, 0.995),
-        "weight_decay": 0.05,
-    },
-    discriminator_scheduler_cls="ReduceLROnPlateau",
-    discriminator_scheduler_params={"patience": 5, "factor": 0.5}
-)
-
-training_piwae_config = CoupledOptimizerTrainerConfig(
-    no_cuda=False,
-    num_epochs=1,
-    keep_best_on_train=True,
-    output_dir=output_dir,
-    per_device_train_batch_size=64,
-    per_device_eval_batch_size=64,
-    encoder_optimizer_cls="Adam",
-    encoder_learning_rate=1e-4,
-    encoder_optimizer_params={
-        "betas": (0.91, 0.995),
-        "weight_decay": 0.05,
-    },
-    encoder_scheduler_cls="ReduceLROnPlateau",
-    encoder_scheduler_params={"patience": 5, "factor": 0.5},
-    decoder_optimizer_cls="Adam",
-    decoder_learning_rate=1e-4,
-    decoder_optimizer_params={
-        "betas": (0.91, 0.995),
-        "weight_decay": 0.05,
-    },
-    decoder_scheduler_cls="ReduceLROnPlateau",
-    decoder_scheduler_params={"patience": 5, "factor": 0.5},
-)
-
-with open("experiments/vae_benchmark/models.txt", "r") as f:
-    all_models = f.read().splitlines()
-
-module = importlib.import_module("pythae.models")
-
-for name, config in zip(all_models[::2], all_models[1::2]):
-
-    print("Training {}...".format(name))
-
+def train_ray(model_configs):
     Model, Config = (
         getattr(module, name),
         getattr(module, config)
     )
+
+    if model_configs is None:
+        model_configs = {}
 
     model_config = Config(
         input_dim = (inp_size,),
         latent_dim = lat_size,
         uses_default_encoder = False,
         uses_default_decoder = False,
+        **model_configs,
     )
     
     mtype = "base"
     if name.lower() == "svae":
         mtype = name
-    encoder = Encoder(inp_size, emb_sizes, lat_size, mtype).to(device)
+
+    ENCODER = Encoder(inp_size, emb_sizes, lat_size, mtype).to(device)
+    DECODER = Decoder(lat_size, emb_sizes, inp_size).to(device)
 
     model = Model(
         model_config = model_config,
-        encoder = encoder,
-        decoder = decoder,
+        encoder = ENCODER,
+        decoder = DECODER,
     )
 
     if name.lower() in ["adversarial_ae", "factorvae"]:
-        training_config = training_aae_config
+        training_config = CONFIGS["adversarial_trainer_config"]
     elif name.lower() == "vaegan":
-        training_config = training_vaegan_config
+        training_config = CONFIGS["coupled_optimizer_adversarial_trainer_config"]
     elif name.lower() == "piwae":
-        training_config = training_piwae_config
+        training_config = CONFIGS["coupled_optimizer_trainer_config"]
     else:
-        training_config = training_base_config
+        training_config = CONFIGS["training_base_config"]
     
-    pipeline = TrainingPipeline(
-    	training_config = training_config,
-    	model = model
-    )
-
-    writer = SummaryWriter(log_dir=os.path.join(ts_output_dir, name))
-    callbacks = [TBLogger(writer),]
-
-    pipeline(
-        train_data = XX,
-        eval_data = XX,
+    trainer = BaseTrainer(
+        model = model,
+        train_dataset = fit_dataset,
+        eval_dataset = val_dataset,
+        training_config = training_config,
         callbacks = callbacks,
+        repeat_number=rn,
+        training_signature=training_signature,
     )
 
-    writer.close()
+    trainer.train()
+    # writer.close()
+
+    del model
+    del trainer
+    del training_config
+    del ENCODER
+    del DECODER
+    with torch.no_grad():
+        torch.cuda.empty_cache()
+    gc.collect()
+
+transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.0,), (1.0,)),
+    transforms.Lambda(torch.flatten),
+])
+
+fit = MNIST("tests/mnist/mnist/", train=True, download=True, transform=transform)
+val = MNIST("tests/mnist/mnist/", train=True, download=True, transform=transform)
+fit_dataset = BaseDataset(fit.data[:1000].flatten(1) / 255., torch.ones((fit.data[:1000].size(0))))
+val_dataset = BaseDataset(val.data[:1000].flatten(1) / 255., torch.ones((val.data[:1000].size(0))))
+
+inp_size = 28*28
+lat_size = 2
+emb_sizes = [inp_size//i for i in range(1, 4)]
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+N_REPEATS = 10
+
+training_signature = str(datetime.datetime.now())[0:19].replace(" ", "_").replace(":", "-")
+path_name = "experiments/vae_benchmark/all_models_logs"
+
+if os.path.exists(path_name):
+    import shutil; shutil.rmtree(path_name) # DEBUG
+
+
+# output_dir = os.path.join(path_name, "tensorboard_"+training_signature)
+# os.makedirs(output_dir, exist_ok=True)
+
+with open("experiments/vae_benchmark/models.txt", "r") as f:
+    all_models = f.read().splitlines()
+
+module = importlib.import_module("pythae.models")
+for rn in range(1, N_REPEATS+1):
+
+    print("="*20, "REPEAT {}/{}".format(rn, N_REPEATS), "="*20)
+    for name, config in zip(all_models[::2], all_models[1::2]):
+        # writer = SummaryWriter(log_dir=os.path.join(output_dir, name))
+        callbacks = [TBLogger()]
+
+        print("Training {}...".format(name))
+        with open("experiments/vae_benchmark/configs/{}.yml".format(name.lower()), "r") as stream:
+            search_space = yaml.safe_load(stream)
+
+        if search_space is not None:
+            print(">>> Tuning {}'s hyperparameters...".format(name))
+            for x in search_space:
+                search_space[x] = tune.uniform(*search_space[x])
+            
+            tuner = tune.Tuner(
+                train_ray,
+                tune_config=tune.TuneConfig(
+                    num_samples=100,
+                    mode="min",
+                    metric="eval_epoch_loss",
+                    # scheduler=ASHAScheduler(stop_last_trials=False),
+                ),
+                param_space=search_space,
+            )
+
+            results = tuner.fit()
+            for i in range(len(results)): 
+                result = results[i].metrics
+                d = dict()
+                d["eval_epoch_loss"] = result["eval_epoch_loss"]
+                for x in result["config"]:
+                    d[x] = result["config"][x]
+                
+                if not i:
+                    df = pd.DataFrame(columns=d.keys())
+
+                df.loc[-1] = d
+                df.index += 1
+
+            df.to_csv(os.path.join(
+                path_name, f"{name}_rep_{rn}_results_{training_signature}.csv"
+            ), sep=";", float_format="%.8f")
+
+        else:
+            continue
+            callbacks = None
+            train_ray(search_space)
+
+    break
--- experiments/vae_benchmark/utils.py
+++ experiments/vae_benchmark/utils.py
@@ -3,21 +3,150 @@ from torch import nn
 
 from pythae.models.nn import BaseEncoder, BaseDecoder
 from pythae.models.base.base_utils import ModelOutput
-from pythae.trainers.training_callbacks import MetricConsolePrinterCallback
+from pythae.trainers.training_callbacks import MetricConsolePrinterCallback, TrainingCallback
 from pythae.trainers.base_trainer.base_training_config import BaseTrainerConfig
 
-class TBLogger(MetricConsolePrinterCallback):
-    def __init__(self, writer):
-        self.writer = writer
-        self.current_indx = 0
+from ray import air, tune
 
-    def on_log(self, training_config: BaseTrainerConfig, logs, **kwargs):
-        epoch_train_loss = logs.get("train_epoch_loss", None)
-        epoch_eval_loss = logs.get("eval_epoch_loss", None)
 
-        self.current_indx += 1
-        self.writer.add_scalar("loss/eval", epoch_eval_loss, self.current_indx)
-        self.writer.add_scalar("loss/train", epoch_train_loss, self.current_indx)
+from pythae.trainers import (
+    BaseTrainerConfig,
+    AdversarialTrainerConfig,
+    CoupledOptimizerAdversarialTrainerConfig,
+    CoupledOptimizerTrainerConfig,
+)
+
+output_dir = "experiments/vae_benchmark/all_models_logs"
+
+NUM_EPOCHS = 100
+
+training_base_config = BaseTrainerConfig(
+    no_cuda=True,
+    num_epochs=NUM_EPOCHS,
+    keep_best_on_train=True,
+    output_dir=output_dir,
+    per_device_train_batch_size=64,
+    per_device_eval_batch_size=64,
+    optimizer_cls="Adam",
+    learning_rate=1e-4,
+    optimizer_params={
+        "betas": (0.91, 0.995),
+        "weight_decay": 0.05,
+    },
+    scheduler_cls="ReduceLROnPlateau",
+    scheduler_params={"patience": 5, "factor": 0.5}
+)
+
+adversarial_trainer_config = AdversarialTrainerConfig(
+    no_cuda=True,
+    num_epochs=NUM_EPOCHS,
+    keep_best_on_train=True,
+    output_dir=output_dir,
+    per_device_train_batch_size=64,
+    per_device_eval_batch_size=64,
+    autoencoder_optimizer_cls="Adam",
+    autoencoder_learning_rate=1e-4,
+    autoencoder_optimizer_params={
+        "betas": (0.91, 0.995),
+        "weight_decay": 0.05,
+    },
+    autoencoder_scheduler_cls="ReduceLROnPlateau",
+    autoencoder_scheduler_params={"patience": 5, "factor": 0.5},
+    discriminator_optimizer_cls="Adam",
+    discriminator_learning_rate=1e-4,
+    discriminator_optimizer_params={
+        "betas": (0.91, 0.995),
+        "weight_decay": 0.05,
+    },
+    discriminator_scheduler_cls="ReduceLROnPlateau",
+    discriminator_scheduler_params={"patience": 5, "factor": 0.5}
+)
+
+coupled_optimizer_adversarial_trainer_config = CoupledOptimizerAdversarialTrainerConfig(
+    no_cuda=True,
+    num_epochs=NUM_EPOCHS,
+    keep_best_on_train=True,
+    output_dir=output_dir,
+    per_device_train_batch_size=64,
+    per_device_eval_batch_size=64,
+    encoder_optimizer_cls="Adam",
+    encoder_learning_rate=1e-4,
+    encoder_optimizer_params={
+        "betas": (0.91, 0.995),
+        "weight_decay": 0.05,
+    },
+    encoder_scheduler_cls="ReduceLROnPlateau",
+    encoder_scheduler_params={"patience": 5, "factor": 0.5},
+    decoder_optimizer_cls="Adam",
+    decoder_learning_rate=1e-4,
+    decoder_optimizer_params={
+        "betas": (0.91, 0.995),
+        "weight_decay": 0.05,
+    },
+    decoder_scheduler_cls="ReduceLROnPlateau",
+    decoder_scheduler_params={"patience": 5, "factor": 0.5},
+    discriminator_optimizer_cls="Adam",
+    discriminator_learning_rate=1e-4,
+    discriminator_optimizer_params={
+        "betas": (0.91, 0.995),
+        "weight_decay": 0.05,
+    },
+    discriminator_scheduler_cls="ReduceLROnPlateau",
+    discriminator_scheduler_params={"patience": 5, "factor": 0.5}
+)
+
+coupled_optimizer_trainer_config = CoupledOptimizerTrainerConfig(
+    no_cuda=True,
+    num_epochs=NUM_EPOCHS,
+    keep_best_on_train=True,
+    output_dir=output_dir,
+    per_device_train_batch_size=64,
+    per_device_eval_batch_size=64,
+    encoder_optimizer_cls="Adam",
+    encoder_learning_rate=1e-4,
+    encoder_optimizer_params={
+        "betas": (0.91, 0.995),
+        "weight_decay": 0.05,
+    },
+    encoder_scheduler_cls="ReduceLROnPlateau",
+    encoder_scheduler_params={"patience": 5, "factor": 0.5},
+    decoder_optimizer_cls="Adam",
+    decoder_learning_rate=1e-4,
+    decoder_optimizer_params={
+        "betas": (0.91, 0.995),
+        "weight_decay": 0.05,
+    },
+    decoder_scheduler_cls="ReduceLROnPlateau",
+    decoder_scheduler_params={"patience": 5, "factor": 0.5},
+)
+
+CONFIGS = {
+    "adversarial_trainer_config": adversarial_trainer_config, 
+    "coupled_optimizer_adversarial_trainer_config": coupled_optimizer_adversarial_trainer_config, 
+    "coupled_optimizer_trainer_config": coupled_optimizer_trainer_config, 
+    "training_base_config": training_base_config, 
+}
+
+
+class TBLogger(TrainingCallback):
+    def __init__(self):
+        super().__init__()
+        # self.writer = writer
+        # self.current_indx = 0
+
+    # def on_log(self, training_config: BaseTrainerConfig, logs, **kwargs):
+    #     epoch_train_loss = logs.get("train_epoch_loss", None)
+    #     epoch_eval_loss = logs.get("eval_epoch_loss", None)
+
+    #     self.current_indx += 1
+    #     self.writer.add_scalar("loss/train", epoch_train_loss, self.current_indx)
+    #     self.writer.add_scalar("loss/eval", epoch_eval_loss, self.current_indx)
+
+
+    def on_epoch_end(self, training_config: BaseTrainerConfig, **kwargs):
+        metrics = kwargs.pop("metrics")
+        tune.report(eval_epoch_loss=metrics["eval_epoch_loss"])
+
 
 class Encoder(BaseEncoder):
     def __init__(self, inp_size, emb_sizes, lat_size, mtype="base"):
@@ -88,3 +217,4 @@ class Decoder(BaseDecoder):
 
         output["reconstruction"] = self.reconstruction(x)
         return output
+
