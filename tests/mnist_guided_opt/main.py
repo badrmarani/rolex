44e6ade7ac835696252f104499e830331727a69f
Badr-Eddine Marani <badr-eddine.marani@michelin.com>
2023-04-21 14:26:10 +0200

    gradient ascent (or descent?)

--- tests/mnist_guided_opt/main.py
+++ tests/mnist_guided_opt/main.py
@@ -3,13 +3,13 @@ from torch import nn
 
 import matplotlib
 import matplotlib.pyplot as plt
+
 matplotlib.rcParams["backend"] = "Qt5Agg"
 
 from mnist_utils import (
     AuxNetwork,
     GuidedVAE,
     get_mnist_loaders,
-    mutual_information_is,
     plot_mutual_information,
 )
 
@@ -20,8 +20,15 @@ args = {
     "inp_size": 28 * 28,
     "emb_sizes": [128, 64, 10],
     "lat_size": 2,
+    "beta": 1.0,
     "add_dropouts": True,
-    "aux_network_path": "tests/mnist_guided_opt/weights/mnist_auxnet_weights.pkl"
+    "n_simulations": 10,
+    "n_sampled_outcomes": 10,
+    "aux_network_path": "tests/mnist_guided_opt/weights/mnist_auxnet_weights.pkl",
+    "n_gradient_steps": 20,
+    "gradient_scale": 0.1,
+    "normalize_gradient": True,
+    "uncertainty_threshold_value": 1000,
 }
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -32,26 +39,38 @@ aux_network.load_state_dict(
     state_dict=torch.load(
         args["aux_network_path"],
         map_location=device,
-)["state_dict"])
+    )["state_dict"]
+)
 
 guided_vae = GuidedVAE(
     inp_size=args["inp_size"],
     emb_sizes=args["emb_sizes"],
     lat_size=args["lat_size"],
+    n_gradient_steps=args["n_gradient_steps"],
+    n_simulations=args["n_simulations"],
+    n_sampled_outcomes=args["n_sampled_outcomes"],
+    gradient_scale=args["gradient_scale"],
+    uncertainty_threshold_value=args["uncertainty_threshold_value"],
     add_dropouts=args["add_dropouts"],
-    aux_network=aux_network,
+    normalize_gradient=args["normalize_gradient"],
+    aux_network=args["aux_network"],
 ).to(device)
 
-x = torch.randn(args["batch_size"], args["inp_size"], device=device)
-xhat, mu, logvar = guided_vae(x)
-print(
-    xhat.size(),
-    mu.size(),
-    logvar.size()
-)
+# x = torch.randn(args["batch_size"], args["inp_size"], device=device)
+# xhat, mu, logvar = guided_vae(x)
+# print(
+#     xhat.size(),
+#     mu.size(),
+#     logvar.size()
+# )
 
 # grid_size = 20
 # step_size = 0.5
 # plot_mutual_information(
-#     guided_vae, grid_size, step_size, device
+#     model=guided_vae,
+#     grid_size=grid_size,
+#     step_size=step_size,
+#     n_simulations=args["n_simulations"],
+#     n_sampled_outcomes=args["n_sampled_outcomes"],
+#     device=device,
 # )
--- tests/mnist_guided_opt/mnist_utils.py
+++ tests/mnist_guided_opt/mnist_utils.py
@@ -9,7 +9,9 @@ from torchvision import transforms
 
 import matplotlib
 import matplotlib.pyplot as plt
+
 matplotlib.rcParams["backend"] = "Qt5Agg"
+
 
 def get_mnist_loaders(batch_size) -> Tuple[data.DataLoader]:
     transform = transforms.Compose(
@@ -43,20 +45,22 @@ def get_mnist_loaders(batch_size) -> Tuple[data.DataLoader]:
 
     return fit_loader, val_loader
 
+
 def loss_function(
     x: torch.Tensor,
     xhat: torch.Tensor,
     mu: torch.Tensor,
     logvar: torch.Tensor,
+    beta: float = 1.0,
 ) -> Tuple[torch.Tensor]:
-    kld_loss = - 0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.pow(2), dim=-1)
+    kld_loss = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.pow(2), dim=-1)
     rec_loss = 0.5 * nn.functional.mse_loss(
         xhat.flatten(1),
         x.flatten(1),
         reduction="none",
     ).sum(-1)
-    
-    loss = kld_loss + rec_loss
+
+    loss = beta * kld_loss + rec_loss
 
     return (
         loss.mean(0),
@@ -64,10 +68,12 @@ def loss_function(
         rec_loss.mean(0),
     )
 
+
 def LDE(log_a, log_b):
     max_log = torch.max(log_a, log_b)
     min_log = torch.min(log_a, log_b)
     return max_log + torch.log(1 + torch.exp(min_log - max_log))
+
 
 @torch.no_grad()
 def mutual_information_is(
@@ -76,52 +82,74 @@ def mutual_information_is(
     n_simulations: int = 10,
     n_sampled_outcomes: int = 10,
 ) -> torch.Tensor:
-        log_mi = []
+    log_mi = []
 
-        network.train()
-        for s in range(n_simulations):
-            all_log_psm = []
-            
-            xhat = network.decoder(z)
-            
-            for _ in range(n_sampled_outcomes):
-                network.eval(); network.enable_dropout()
-                
-                xxhat = network.decoder(z)
-                log_psm = - nn.functional.gaussian_nll_loss(
-                    xxhat,
-                    xhat,
-                    torch.ones_like(xhat, device=xhat.device),
-                    reduction="none"
-                ).sum(-1)
+    network.train()
+    for s in range(n_simulations):
+        all_log_psm = []
 
-                all_log_psm.append(log_psm)
+        xhat = network.decoder(z)
 
-            all_log_psm = torch.stack(all_log_psm, dim=1)
-            log_ps = - torch.log(torch.tensor(n_sampled_outcomes).float()) + torch.logsumexp(all_log_psm, dim=1)
-            
-            right_log_hs = log_ps + torch.log(-log_ps)
-            psm_log_psm = all_log_psm + torch.log(-all_log_psm)
-            left_log_hs = - torch.log(torch.tensor(n_sampled_outcomes).float()) + torch.logsumexp(psm_log_psm, dim=1)
+        for _ in range(n_sampled_outcomes):
+            network.eval()
+            network.enable_dropout()
 
-            tmp_log_hs = LDE(left_log_hs, right_log_hs) - log_ps
-            log_mi.append(tmp_log_hs)
+            xxhat = network.decoder(z)
+            log_psm = -nn.functional.gaussian_nll_loss(
+                xxhat,
+                xhat,
+                torch.ones_like(xhat, device=xhat.device),
+                reduction="none",
+                full=True,
+            ).sum(-1)
 
-        log_mi = torch.stack(log_mi, dim=1)
-        log_mi_avg = - torch.log(torch.tensor(n_simulations).float()) + torch.logsumexp(log_mi, dim=1)
-        
-        return log_mi_avg.exp()
+            all_log_psm.append(log_psm)
 
-def plot_mutual_information(model, grid_size, step_size, device=None):
+        all_log_psm = torch.stack(all_log_psm, dim=1)
+        log_ps = -torch.log(torch.tensor(n_sampled_outcomes).float()) + torch.logsumexp(
+            all_log_psm, dim=1
+        )
+
+        right_log_hs = log_ps + torch.log(-log_ps)
+        psm_log_psm = all_log_psm + torch.log(-all_log_psm)
+        left_log_hs = -torch.log(
+            torch.tensor(n_sampled_outcomes).float()
+        ) + torch.logsumexp(psm_log_psm, dim=1)
+
+        tmp_log_hs = LDE(left_log_hs, right_log_hs) - log_ps
+        log_mi.append(tmp_log_hs)
+
+    log_mi = torch.stack(log_mi, dim=1)
+    log_mi_avg = -torch.log(torch.tensor(n_simulations).float()) + torch.logsumexp(
+        log_mi, dim=1
+    )
+
+    return log_mi_avg.exp()
+
+
+def plot_mutual_information(
+    model,
+    grid_size,
+    step_size,
+    n_simulations,
+    n_sampled_outcomes,
+    device=None,
+):
     if device is None:
         device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    z1_space = torch.arange(-grid_size, grid_size, step_size, dtype=torch.float, device=device)
-    z2_space = torch.arange(-grid_size, grid_size, step_size, dtype=torch.float, device=device)
+    z1_space = torch.arange(
+        -grid_size, grid_size, step_size, dtype=torch.float, device=device
+    )
+    z2_space = torch.arange(
+        -grid_size, grid_size, step_size, dtype=torch.float, device=device
+    )
     z1, z2 = torch.meshgrid(z1_space, z2_space, indexing="xy")
 
     mi = mutual_information_is(
         model,
         torch.stack((z1.flatten(), z2.flatten()), dim=1),
+        n_simulations=n_simulations,
+        n_sampled_outcomes=n_sampled_outcomes,
     ).view(z1.size())
 
     plt.figure()
@@ -153,6 +181,7 @@ class AuxNetwork(nn.Module):
         x = x.view(x.size(0), -1)
         return torch.log_softmax(self.seq(x), dim=1)
 
+
 class Encoder(nn.Module):
     def __init__(self, inp_size, emb_sizes, lat_size, add_dropouts):
         super().__init__()
@@ -170,13 +199,14 @@ class Encoder(nn.Module):
 
         self.mu = nn.Linear(pre_emb_size, lat_size)
         self.logvar = nn.Linear(pre_emb_size, lat_size)
-        
+
     def forward(self, x: torch.Tensor) -> Tuple[torch.Tensor]:
         embeddings = self.seq(x)
         return (
             self.mu(embeddings),
             self.logvar(embeddings),
         )
+
 
 class Decoder(nn.Module):
     def __init__(self, lat_size, emb_sizes, out_size, add_dropouts):
@@ -190,14 +220,15 @@ class Decoder(nn.Module):
             if i < len(inv_emb_sizes) - 1:
                 seq += [nn.ReLU()]
                 if add_dropouts:
-                    seq += [nn.Dropout(0.5)]     
+                    seq += [nn.Dropout(0.5)]
 
             pre_emb_size = emb_size
 
         self.seq = nn.Sequential(*seq)
-        
+
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         return self.seq(x)
+
 
 class GuidedVAE(nn.Module):
     def __init__(
@@ -205,13 +236,27 @@ class GuidedVAE(nn.Module):
         inp_size: int,
         emb_sizes: int,
         lat_size: int,
+        n_gradient_steps: int,
+        n_simulations: int,
+        n_sampled_outcomes: int,
+        gradient_scale: float,
+        uncertainty_threshold_value: float,
         add_dropouts: bool,
+        normalize_gradient: bool,
         aux_network: nn.Module,
     ) -> None:
         super().__init__()
 
         self.encoder = Encoder(inp_size, emb_sizes, lat_size, add_dropouts)
         self.decoder = Decoder(lat_size, emb_sizes, inp_size, add_dropouts)
+        self.aux_network = aux_network
+
+        self.n_gradient_steps = n_gradient_steps
+        self.n_simulations = n_simulations
+        self.n_sampled_outcomes = n_sampled_outcomes
+        self.normalize_gradient = normalize_gradient
+        self.gradient_scale = gradient_scale
+        self.uncertainty_threshold_value = uncertainty_threshold_value
 
     def enable_dropout(self):
         for m in self.modules():
@@ -226,7 +271,7 @@ class GuidedVAE(nn.Module):
     @torch.no_grad()
     def reconstruct(self, tensor):
         return self.forward(tensor)[0]
-    
+
     @torch.no_grad()
     def generate(self, n_samples, device):
         z = torch.randn((n_samples, self.lat_size), device=device)
@@ -235,13 +280,32 @@ class GuidedVAE(nn.Module):
     def forward(self, x: torch.Tensor) -> Tuple[torch.Tensor]:
         x = x.view(x.size(0), -1)
         mu, logvar = self.encoder(x)
-        z = self.rsample(mu, logvar)
+        z = self.gradient_ascent_optimisation(z=self.rsample(mu, logvar), x=x)
         xhat = self.decoder(z)
         return xhat, mu, logvar
-    
+
     def gradient_ascent_optimisation(
         self,
         x: torch.Tensor,
         z: torch.Tensor,
     ) -> torch.Tensor:
-        pass
+        for step in range(self.n_gradient_steps):
+            P = self.aux_network(z)
+            gradient = torch.autograd.grad(
+                outputs=P,
+                inputs=z,
+                grad_outputs=torch.ones_like(P, device=z.device),
+                retain_graph=False,
+            )[0]
+
+            if self.normalize_gradient:
+                gradient /= gradient.norm(2)
+
+            updated_z = z + gradient * self.gradient_scale
+            mi = mutual_information_is(z)
+
+            mask = mi <= self.uncertainty_threshold_value
+            mask = mask.unsqueeze(-1).repeat(1, 2)
+            updated_z = torch.where(mask, updated_z, z)
+
+        return updated_z
