7d402e292bccac1d852f81053bdbafec452c6750
Badr-Eddine Marani <badr-eddine.marani@michelin.com>
2023-04-24 19:27:28 +0200

    smallish (?) update

--- experiments/vae_benchmark/main.py
+++ experiments/vae_benchmark/main.py
@@ -5,6 +5,9 @@ import importlib
 import os
 import warnings
 
+warnings.filterwarnings("ignore")
+
+import numpy as np
 import pandas as pd
 import torch
 import yaml
@@ -14,172 +17,88 @@ from pythae.pipelines import TrainingPipeline
 from pythae.trainers import BaseTrainer, BaseTrainerConfig
 from ray import air, tune
 from ray.tune.schedulers import ASHAScheduler
+from sklearn.model_selection import train_test_split
 from torch.utils import data
 from torch.utils.tensorboard import SummaryWriter
 from torchvision import transforms
 from torchvision.datasets import MNIST
-
-from sklearn.model_selection import train_test_split
-
-import numpy as np
-
 from utils import *
+from base_model import Encoder, Decoder
 
-warnings.filterwarnings("ignore")
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-def train_ray(model_configs):
-    Model, Config = (
+data = get_dataset(args["dataset_filename"], test_size=0.10, random_state=42)
+train_data = data["train"]["torch"]
+test_data = data["test"]["torch"]
+inp_size = train_data.data.size(1)
+emb_sizes = [inp_size//i for i in range(1, 4)]
+lat_size = args["lat_size"]
+
+
+save_models_dirname = os.path.join(dirname, "all_models_logs")
+save_tensorboard_dirname = os.path.join(save_models_dirname, "tensorboard_"+training_signature)
+os.makedirs(save_models_dirname, exist_ok=True)
+os.makedirs(save_tensorboard_dirname, exist_ok=True)
+
+# if os.path.exists(save_models_dirname):
+#     import shutil; shutil.rmtree(save_models_dirname) # DEBUG
+
+with open(os.path.join(dirname, "models.txt"), "r") as f:
+    all_models = f.read().splitlines()
+
+module = importlib.import_module("pythae.models")
+for name, config in zip(all_models[::2], all_models[1::2]):
+    writer = SummaryWriter(log_dir=os.path.join(
+        save_tensorboard_dirname, name
+    ))
+
+    print("="*20, "Training {}...".format(name), "="*20)
+    callbacks = [TBLogger(writer)]
+    my_model, my_config = (
         getattr(module, name),
         getattr(module, config)
     )
 
-    if model_configs is None:
-        model_configs = {}
-
-    model_config = Config(
+    model_config = my_config(
         input_dim = (inp_size,),
         latent_dim = lat_size,
         uses_default_encoder = False,
         uses_default_decoder = False,
-        **model_configs,
     )
     
-    mtype = "base"
+    encoder_type = "base"
     if name.lower() == "svae":
-        mtype = name
+        encoder_type = name
 
-    ENCODER = Encoder(inp_size, emb_sizes, lat_size, mtype).to(device)
-    DECODER = Decoder(lat_size, emb_sizes, inp_size).to(device)
+    encoder = Encoder(inp_size, emb_sizes, lat_size, encoder_type).to(device)
+    decoder = Decoder(lat_size, emb_sizes, inp_size).to(device)
 
-    model = Model(
+    model = my_model(
         model_config = model_config,
-        encoder = ENCODER,
-        decoder = DECODER,
+        encoder = encoder,
+        decoder = decoder,
     )
 
-    if name.lower() in ["adversarial_ae", "factorvae"]:
-        training_config = CONFIGS["adversarial_trainer_config"]
-    elif name.lower() == "vaegan":
-        training_config = CONFIGS["coupled_optimizer_adversarial_trainer_config"]
-    elif name.lower() == "piwae":
-        training_config = CONFIGS["coupled_optimizer_trainer_config"]
-    else:
-        training_config = CONFIGS["training_base_config"]
-    
+    training_config = get_configs(name)    
     trainer = BaseTrainer(
         model = model,
-        train_dataset = fit_dataset,
-        eval_dataset = val_dataset,
+        train_dataset = train_data,
+        eval_dataset = test_data,
         training_config = training_config,
         callbacks = callbacks,
-        repeat_number=rn,
+        repeat_number=args["n_repeats"],
         training_signature=training_signature,
     )
 
     trainer.train()
-    save_sdmetrics(model, df.sample(1000), f"{name}_{training_signature}")
+    # save_quality_report(model, data["eval"]["pandas"], name)
     writer.close()
 
     del model
     del trainer
     del training_config
-    del ENCODER
-    del DECODER
+    del encoder
+    del decoder
     with torch.no_grad():
         torch.cuda.empty_cache()
     gc.collect()
-
-transform = transforms.Compose([
-    transforms.ToTensor(),
-    transforms.Normalize((0.0,), (1.0,)),
-    transforms.Lambda(torch.flatten),
-])
-
-# fit = MNIST("tests/mnist/mnist/", train=True, download=True, transform=transform)
-# val = MNIST("tests/mnist/mnist/", train=True, download=True, transform=transform)
-# fit_dataset = BaseDataset(fit.data[:1000].flatten(1) / 255., torch.ones((fit.data[:1000].size(0))))
-# val_dataset = BaseDataset(val.data[:1000].flatten(1) / 255., torch.ones((val.data[:1000].size(0))))
-
-df = pd.read_csv("data/full_all_dataset_x.csv", sep=",")
-n_samples = df.shape[0]
-n_features = df.shape[1]
-fit_idx, val_idx = train_test_split(np.arange(0, n_samples, 1), shuffle=True)
-
-fit = torch.from_numpy(df.iloc[fit_idx, :].values).to(torch.float)
-val = torch.from_numpy(df.iloc[val_idx, :].values).to(torch.float)
-
-fit_dataset = BaseDataset(fit, torch.ones(n_samples,))
-val_dataset = BaseDataset(val, torch.ones(n_samples,))
-
-inp_size = n_features
-lat_size = 2
-emb_sizes = [inp_size//i for i in range(1, 4)]
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-N_REPEATS = 1
-
-training_signature = str(datetime.datetime.now())[0:19].replace(" ", "_").replace(":", "-")
-path_name = "experiments/vae_benchmark/all_models_logs"
-
-if os.path.exists(path_name):
-    import shutil; shutil.rmtree(path_name) # DEBUG
-
-
-output_dir = os.path.join(path_name, "tensorboard_"+training_signature)
-os.makedirs(output_dir, exist_ok=True)
-
-with open("experiments/vae_benchmark/models.txt", "r") as f:
-    all_models = f.read().splitlines()
-
-module = importlib.import_module("pythae.models")
-for rn in range(1, N_REPEATS+1):
-
-    print("="*20, "REPEAT {}/{}".format(rn, N_REPEATS), "="*20)
-    for name, config in zip(all_models[::2], all_models[1::2]):
-        writer = SummaryWriter(log_dir=os.path.join(output_dir, name))
-        callbacks = [RayLogger()]
-
-        print("Training {}...".format(name))
-        with open("experiments/vae_benchmark/configs/{}.yml".format(name.lower()), "r") as stream:
-            search_space = yaml.safe_load(stream)
-            search_space = None
-
-        if search_space is not None:
-            print(">>> Tuning {}'s hyperparameters...".format(name))
-            for x in search_space:
-                search_space[x] = tune.uniform(*search_space[x])
-            
-            tuner = tune.Tuner(
-                train_ray,
-                tune_config=tune.TuneConfig(
-                    num_samples=100,
-                    mode="min",
-                    metric="eval_epoch_loss",
-                    # scheduler=ASHAScheduler(stop_last_trials=False),
-                ),
-                param_space=search_space,
-            )
-
-            results = tuner.fit()
-            for i in range(len(results)): 
-                result = results[i].metrics
-                d = dict()
-                d["eval_epoch_loss"] = result["eval_epoch_loss"]
-                for x in result["config"]:
-                    d[x] = result["config"][x]
-                
-                if not i:
-                    df = pd.DataFrame(columns=d.keys())
-
-                df.loc[-1] = d
-                df.index += 1
-
-            df.to_csv(os.path.join(
-                path_name, f"{name}_rep_{rn}_results_{training_signature}.csv"
-            ), sep=";", float_format="%.8f")
-
-        else:
-            callbacks = [TBLogger(writer)]
-            train_ray(search_space)
-
-    break
--- experiments/vae_benchmark/utils.py
+++ experiments/vae_benchmark/utils.py
@@ -1,6 +1,9 @@
 import torch
 from torch import nn
 
+import numpy as np
+
+from pythae.data.datasets import BaseDataset
 from pythae.models import BaseAE
 from pythae.models.nn import BaseEncoder, BaseDecoder
 from pythae.models.base.base_utils import ModelOutput
@@ -14,104 +17,114 @@ from pythae.trainers import (
     CoupledOptimizerTrainerConfig,
 )
 
+import warnings
+warnings.filterwarnings("ignore")
+
 from ray import air, tune
 
-from sdmetrics.single_column import BoundaryAdherence
-from sdmetrics.reports import utils
 from sdmetrics.reports.single_table import QualityReport
 from sdv.metadata import SingleTableMetadata
+
+from sklearn.model_selection import train_test_split
 
 import pandas as pd
 import os
 import yaml
+import datetime
 
+training_signature = str(datetime.datetime.now())[0:19].replace(" ", "_").replace(":", "-")
 dirname = "experiments/vae_benchmark"
-with open(os.path.join("training_config.yml"), "r") as stream:
-    args = yaml.safe_load(stream)
 
-training_base_config = BaseTrainerConfig(
-    no_cuda=args["no_cuda"],
-    num_epochs=args["n_epochs"],
-    keep_best_on_train=args["keep_best_on_train"],
-    output_dir=args["output_dir"],
-    per_device_train_batch_size=args["train_batch_size"],
-    per_device_eval_batch_size=args["train_batch_size"],
-    optimizer_cls=args["optimizer_name"],
-    learning_rate=args["lr"],
-    optimizer_params=args["optimizer_params"],
-    scheduler_cls=args["scheduler_name"],
-    scheduler_params=args["scheduler_params"]
-)
+with open(os.path.join(dirname, "training_config.yml"), "r") as stream:
+    args = yaml.load(stream, Loader=yaml.FullLoader)
 
-adversarial_trainer_config = AdversarialTrainerConfig(
-    no_cuda=args["no_cuda"],
-    num_epochs=args["n_epochs"],
-    keep_best_on_train=args["keep_best_on_train"],
-    output_dir=args["output_dir"],
-    per_device_train_batch_size=args["train_batch_size"],
-    per_device_eval_batch_size=args["train_batch_size"],
-    autoencoder_optimizer_cls=args["optimizer_name"],
-    autoencoder_learning_rate=args["lr"],
-    autoencoder_optimizer_params=args["optimizer_params"],
-    autoencoder_scheduler_cls=args["scheduler_name"],
-    autoencoder_scheduler_params=args["scheduler_params"],
-    discriminator_optimizer_cls=args["optimizer_name"],
-    discriminator_learning_rate=args["lr"],
-    discriminator_optimizer_params=args["optimizer_params"],
-    discriminator_scheduler_cls=args["scheduler_name"],
-    discriminator_scheduler_params=args["scheduler_params"]
-)
 
-coupled_optimizer_adversarial_trainer_config = CoupledOptimizerAdversarialTrainerConfig(
-    no_cuda=args["no_cuda"],
-    num_epochs=args["n_epochs"],
-    keep_best_on_train=args["keep_best_on_train"],
-    output_dir=args["output_dir"],
-    per_device_train_batch_size=args["train_batch_size"],
-    per_device_eval_batch_size=args["eval_batch_size"],
-    encoder_optimizer_cls=args["optimizer_name"],
-    encoder_learning_rate=args["lr"],
-    encoder_optimizer_params=args["optimizer_params"],
-    encoder_scheduler_cls=args["scheduler_name"],
-    encoder_scheduler_params=args["scheduler_params"],
-    decoder_optimizer_cls=args["optimizer_name"],
-    decoder_learning_rate=args["lr"],
-    decoder_optimizer_params=args["optimizer_params"],
-    decoder_scheduler_cls=args["scheduler_name"],
-    decoder_scheduler_params=args["scheduler_params"],
-    discriminator_optimizer_cls=args["optimizer_name"],
-    discriminator_learning_rate=args["lr"],
-    discriminator_optimizer_params=args["optimizer_params"],
-    discriminator_scheduler_cls=args["scheduler_name"],
-    discriminator_scheduler_params=args["scheduler_params"]
-)
+def get_configs(model_name:str):
+    training_base_config = BaseTrainerConfig(
+        no_cuda=args["no_cuda"],
+        num_epochs=args["n_epochs"],
+        keep_best_on_train=args["keep_best_on_train"],
+        output_dir=args["output_dir"],
+        per_device_train_batch_size=args["train_batch_size"],
+        per_device_eval_batch_size=args["train_batch_size"],
+        optimizer_cls=args["optimizer_name"],
+        learning_rate=args["lr"],
+        optimizer_params=args["optimizer_params"],
+        scheduler_cls=args["scheduler_name"],
+        scheduler_params=args["scheduler_params"]
+    )
 
-coupled_optimizer_trainer_config = CoupledOptimizerTrainerConfig(
-    no_cuda=args["no_cuda"],
-    num_epochs=args["n_epochs"],
-    keep_best_on_train=args["keep_best_on_train"],
-    output_dir=args["output_dir"],
-    per_device_train_batch_size=args["train_batch_size"],
-    per_device_eval_batch_size=args["eval_batch_size"],
-    encoder_optimizer_cls=args["optimizer_name"],
-    encoder_learning_rate=args["lr"],
-    encoder_optimizer_params=args["optimizer_params"],
-    encoder_scheduler_cls=args["scheduler_name"],
-    encoder_scheduler_params=args["scheduler_params"],
-    decoder_optimizer_cls=args["optimizer_name"],
-    decoder_learning_rate=args["lr"],
-    decoder_optimizer_params=args["optimizer_params"],
-    decoder_scheduler_cls=args["scheduler_name"],
-    decoder_scheduler_params=args["scheduler_params"],
-)
+    adversarial_trainer_config = AdversarialTrainerConfig(
+        no_cuda=args["no_cuda"],
+        num_epochs=args["n_epochs"],
+        keep_best_on_train=args["keep_best_on_train"],
+        output_dir=args["output_dir"],
+        per_device_train_batch_size=args["train_batch_size"],
+        per_device_eval_batch_size=args["train_batch_size"],
+        autoencoder_optimizer_cls=args["optimizer_name"],
+        autoencoder_learning_rate=args["lr"],
+        autoencoder_optimizer_params=args["optimizer_params"],
+        autoencoder_scheduler_cls=args["scheduler_name"],
+        autoencoder_scheduler_params=args["scheduler_params"],
+        discriminator_optimizer_cls=args["optimizer_name"],
+        discriminator_learning_rate=args["lr"],
+        discriminator_optimizer_params=args["optimizer_params"],
+        discriminator_scheduler_cls=args["scheduler_name"],
+        discriminator_scheduler_params=args["scheduler_params"]
+    )
 
-CONFIGS = {
-    "adversarial_trainer_config": adversarial_trainer_config, 
-    "coupled_optimizer_adversarial_trainer_config": coupled_optimizer_adversarial_trainer_config, 
-    "coupled_optimizer_trainer_config": coupled_optimizer_trainer_config, 
-    "training_base_config": training_base_config, 
-}
+    coupled_optimizer_adversarial_trainer_config = CoupledOptimizerAdversarialTrainerConfig(
+        no_cuda=args["no_cuda"],
+        num_epochs=args["n_epochs"],
+        keep_best_on_train=args["keep_best_on_train"],
+        output_dir=args["output_dir"],
+        per_device_train_batch_size=args["train_batch_size"],
+        per_device_eval_batch_size=args["eval_batch_size"],
+        encoder_optimizer_cls=args["optimizer_name"],
+        encoder_learning_rate=args["lr"],
+        encoder_optimizer_params=args["optimizer_params"],
+        encoder_scheduler_cls=args["scheduler_name"],
+        encoder_scheduler_params=args["scheduler_params"],
+        decoder_optimizer_cls=args["optimizer_name"],
+        decoder_learning_rate=args["lr"],
+        decoder_optimizer_params=args["optimizer_params"],
+        decoder_scheduler_cls=args["scheduler_name"],
+        decoder_scheduler_params=args["scheduler_params"],
+        discriminator_optimizer_cls=args["optimizer_name"],
+        discriminator_learning_rate=args["lr"],
+        discriminator_optimizer_params=args["optimizer_params"],
+        discriminator_scheduler_cls=args["scheduler_name"],
+        discriminator_scheduler_params=args["scheduler_params"]
+    )
 
+    coupled_optimizer_trainer_config = CoupledOptimizerTrainerConfig(
+        no_cuda=args["no_cuda"],
+        num_epochs=args["n_epochs"],
+        keep_best_on_train=args["keep_best_on_train"],
+        output_dir=args["output_dir"],
+        per_device_train_batch_size=args["train_batch_size"],
+        per_device_eval_batch_size=args["eval_batch_size"],
+        encoder_optimizer_cls=args["optimizer_name"],
+        encoder_learning_rate=args["lr"],
+        encoder_optimizer_params=args["optimizer_params"],
+        encoder_scheduler_cls=args["scheduler_name"],
+        encoder_scheduler_params=args["scheduler_params"],
+        decoder_optimizer_cls=args["optimizer_name"],
+        decoder_learning_rate=args["lr"],
+        decoder_optimizer_params=args["optimizer_params"],
+        decoder_scheduler_cls=args["scheduler_name"],
+        decoder_scheduler_params=args["scheduler_params"],
+    )
+
+    if model_name.lower() in ["adversarial_ae", "factorvae"]:
+        training_config = adversarial_trainer_config
+    elif model_name.lower() in ["vaegan"]:
+        training_config = coupled_optimizer_adversarial_trainer_config
+    elif model_name.lower() in ["piwae"]:
+        training_config = coupled_optimizer_trainer_config
+    else:
+        training_config = training_base_config
+    return training_config
 
 class TBLogger(MetricConsolePrinterCallback):
     def __init__(self, writer):
@@ -136,35 +149,95 @@ class RayLogger(TrainingCallback):
         metrics = kwargs.pop("metrics")
         tune.report(eval_epoch_loss=metrics["eval_epoch_loss"])
 
-def save_sdmetrics(model: BaseAE, dataset_x: pd.DataFrame, name):
-    normal_sampler = NormalSampler(model)    
-    sampled_x = normal_sampler.sample(num_samples=dataset_x.shape[0])
+def save_quality_report(model: BaseAE, dataset: pd.DataFrame, name):
+    normal_sampler = NormalSampler(model)
+    fake_dataset = normal_sampler.sample(num_samples=dataset.shape[0])
+
+    binary_columns = [col for col in dataset.columns if dataset[col].dtype == "int64"]
+    dataset[binary_columns] = dataset[binary_columns].astype("category")
 
     metadata = SingleTableMetadata()
-    metadata.detect_from_dataframe(dataset_x)
+    metadata.detect_from_dataframe(dataset)
 
     quality_report = QualityReport()
-    df_sampled_x = pd.DataFrame(
-        sampled_x.detach().cpu().numpy(),
-        columns=dataset_x.columns,
-        index=dataset_x.index
+    df_fake_dataset = pd.DataFrame(
+        fake_dataset.detach().cpu().numpy(),
+        columns=dataset.columns,
+        index=dataset.index
     )
 
-    quality_report.generate(dataset_x, df_sampled_x, metadata.to_dict())
+    quality_report.generate(dataset, df_fake_dataset, metadata.to_dict())
+    quality_report.save(name)
+
+def prepare_dataset(filename: str):
+    data = pd.read_csv(filename, sep=",")
+    data.reset_index()
+    data = data.iloc[:, 2:-6]
     
-    fig = quality_report.get_visualization(property_name="Column Shapes")
-    fig.write_image(os.path.join(args["output_dir"], name + "_column_shapes.jpg"), width=800, height=800, scale=6)
+    # remove columns with nan values
+    tmp = data.isna().any()
+    na_columns = tmp[lambda x: x].index.to_list()
+    data.drop(columns=na_columns, axis=1, inplace=True)
+    return data
 
-    fig = quality_report.get_visualization(property_name="Column Pair Trends")
-    fig.write_image(os.path.join(args["output_dir"], name + "_column_pair_trends.jpg"), width=800, height=800, scale=6)
+def get_dataset(filename: str, **kwargs):
+    data = prepare_dataset(filename)
+    n_samples = data.shape[0]
+    idx = np.arange(0, n_samples, 1)
+    train_idx, eval_idx = train_test_split(idx, **kwargs)
+    train_idx, test_idx = train_test_split(train_idx, **kwargs)
+    train = torch.from_numpy(data.iloc[train_idx,:].values).to(torch.float)
+    test = torch.from_numpy(data.iloc[test_idx,:].values).to(torch.float)
+    eval = torch.from_numpy(data.iloc[eval_idx,:].values).to(torch.float)
+
+    return {
+        "train": {
+            "pandas": data.iloc[train_idx,:],
+            "torch": BaseDataset(train, torch.ones(n_samples,)),
+        },
+        "test": {
+            "pandas": data.iloc[test_idx,:],
+            "torch": BaseDataset(test, torch.ones(n_samples,)),
+        },
+        "eval": {
+            "pandas": data.iloc[eval_idx,:],
+            "torch": BaseDataset(eval, torch.ones(n_samples,)),
+        },
+    }
 
 
-    fig = utils.get_column_plot(
-        real_data=dataset_x,
-        synthetic_data=df_sampled_x,
-        column_name="data_054",
-        metadata=metadata.to_dict(),
-    )
-    fig.write_image(os.path.join(args["output_dir"], name + "_get_column_plot.jpg"), width=800, height=800, scale=6)
+def get_stats_per_model(model: BaseAE, dataset: pd.DataFrame, name):
+    normal_sampler = NormalSampler(model)
+    fake_dataset = normal_sampler.sample(num_samples=dataset.shape[0])
 
-    quality_report.save(os.path.join(args["output_dir"], name + "quality_report.pkl"))
+    nll = model.get_nll(model, dataset, n_samples=3)
+
+
+from pythae.models import AutoModel
+from pathlib import Path
+import numpy as np
+
+def get_stats(dirname: str, dataset, repeat_id):
+    all_model_dir = [x for x in os.listdir(dirname) if not x.startswith("tensorboard")]
+    for model_dir in all_model_dir:
+        model_name = model_dir.split("_training")[0]
+        path = Path(
+            dirname,
+            model_dir,
+            model_name + f"_rep_{repeat_id}",
+            "final_model/",
+        )
+
+        trained_model = AutoModel.load_from_folder(path)
+        quality_report_save_path = os.path.join(
+            dirname,
+            model_dir,
+            model_name + f"_rep_{repeat_id}",
+            model_name+"_quality_report.pkl",
+        )
+        save_quality_report(trained_model, dataset, quality_report_save_path)
+
+# dirname = "experiments/vae_benchmark/all_models_logs"
+# dataset = get_dataset("data/fulldataset.csv", test_size=0.10, random_state=42)
+# eval = dataset["eval"]["pandas"]
+# get_stats(dirname, eval, 1)
