0cb53c87cd40bfcb33aced9154e22d5cc6acce19
Badr-Eddine Marani <badr-eddine.marani@outlook.com>
2023-06-05 17:28:30 +0200

    update dataset

--- uqvae/dataset.py
+++ uqvae/dataset.py
@@ -1,15 +1,13 @@
-import os
 import warnings
 
 warnings.filterwarnings("ignore")
 
+import numpy as np
 import pandas as pd
 import torch
-from ctgan.data_transformer import DataTransformer
-from sdv.metadata import SingleTableMetadata
-from sdv.single_table.utils import detect_discrete_columns
 from sklearn.model_selection import train_test_split
 from torch.utils import data
+from utils import reproduce
 
 
 def split_dataset(df: pd.DataFrame):
@@ -20,86 +18,25 @@ def split_dataset(df: pd.DataFrame):
     )
 
 
-def fit_val_split(
-    tensor,
-    train_size: float = 0.7,
-    shuffle: bool = True,
-):
+def tensor_train_test_split(tensor: torch.Tensor, **kwargs):
     indx = torch.arange(0, tensor.size(0) - 1, 1)
-    fit_indx, val_indx = train_test_split(indx, train_size=train_size, shuffle=shuffle)
-    return (
-        data.TensorDataset(tensor[fit_indx, :-2], tensor[fit_indx, -2:]),
-        data.TensorDataset(tensor[val_indx, :-2], tensor[val_indx, -2:]),
+    fit_indx, val_indx = train_test_split(indx, **kwargs)
+    return tensor[fit_indx, :], tensor[val_indx, :]
+
+
+def make_loader(dataset: np.array, seed: int = 42, shuffle: bool = False):
+    if shuffle:
+        np.random.shuffle(dataset)
+
+    generator = torch.Generator()
+    generator.manual_seed(seed)
+
+    loader = data.DataLoader(
+        data.TensorDataset(torch.from_numpy(dataset.astype("float32"))),
+        batch_size=500,
+        shuffle=False,
+        worker_init_fn=reproduce,
+        generator=generator,
     )
 
-
-def prepare_quality_dataset(
-    dataset: pd.DataFrame,
-    targets: pd.DataFrame,
-    train_size: float = 0.7,
-    shuffle: bool = True,
-    save_metadata: bool = None,
-    transform: bool = True,
-):
-    if transform:
-        metadata = SingleTableMetadata()
-        metadata.detect_from_dataframe(dataset)
-
-        binary_columns = [col for col in dataset.columns if dataset[col].dtype == "int64"]
-
-        for feat in binary_columns:
-            metadata.update_column(
-                column_name=feat,
-                sdtype="categorical",
-            )
-        if save_metadata is not None:
-            if os.path.exists(save_metadata):
-                os.remove(save_metadata)
-            metadata.save_to_json(filepath=save_metadata)
-
-        discrete_columns = detect_discrete_columns(metadata, dataset)
-        transformer = DataTransformer()
-        transformer.fit(dataset, discrete_columns)
-
-        new_data = transformer.transform(dataset)
-        new_data = torch.from_numpy(new_data)
-    else:
-        transformer = None
-        new_data = torch.from_numpy(dataset.values.astype("float"))
-
-    targets = torch.from_numpy(targets.values.astype("float"))
-    all_tensors = torch.cat((new_data, targets), dim=-1)
-    fit, val = fit_val_split(all_tensors, train_size=train_size, shuffle=shuffle)
-
-    return (
-        transformer,
-        fit,
-        val,
-    )
-
-
-def clean_dataset(df: pd.DataFrame):
-    dataset = df.copy()
-    targets = dataset[["target_000", "target_001"]]
-
-    notna_targets_indx = targets.notna().all(1)
-    dataset = dataset[notna_targets_indx]
-
-    dataset.drop(columns=["Unnamed: 0", "ROW_ID"], axis=1, inplace=True)
-    dataset.dropna(
-        axis="columns",
-        how="any",
-        inplace=True,
-    )
-
-    binary_columns = [col for col in dataset.columns if dataset[col].dtype == "int64"]
-    one_value_cols = pd.Series(
-        {
-            c: dataset[c].unique()
-            for c in dataset[binary_columns]
-            if len(dataset[c].unique()) == 1
-        }
-    ).index.to_list()
-
-    dataset.drop(columns=one_value_cols, axis=1, inplace=True)
-    return dataset
+    return loader
